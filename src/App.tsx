01a4380eb608de1d456de295d446fac65dbcc231
Luis Carlos <luiscarlossilva.dev@gmail.com>
2020-08-19 17:50:13 -0300

    criando components e os icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout'
 import GlobalStyles from './styles/GlobalStyles'
+
 
 function App() {
   return (
     <>
+      <Layout />
       <GlobalStyles />
-      <h1>hello world</h1>
     </>
   );
 }
---
+++ src/styles/Icons.ts
@@ -0,0 +1,13 @@
+export {
+  MdHome,
+  MdNotifications,
+  MdEmail,
+  MdFavoriteBorder,
+  MdExitToApp,
+  MdLocationOn,
+  MdSearch,
+} from 'react-icons/md';
+
+export { MdPerson, MdCake, MdFavorite } from 'react-icons/md';
+export { BsArrowLeftShort } from 'react-icons/bs';
+export { MdChat } from 'react-icons/md';
